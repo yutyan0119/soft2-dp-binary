1161741862010ffb57d65da85d31dc31d0d6ec0f
Yuto Nakamura <maron07222010@gmail.com>
2022-01-07 23:47:10 +0900

    clang format

--- knapsack/include/item.h
+++ knapsack/include/item.h
@@ -4,8 +4,8 @@
 // 価値valueと重さweightが格納されている
 
 typedef struct item {
-    double value;
-    double weight;
+  double value;
+  double weight;
 } Item;
 
 // 構造体 Item
@@ -15,7 +15,6 @@ typedef struct itemset {
   size_t number;
   Item *item;
 } Itemset;
-
 
 // Itemsetを初期化し、そのポインタを返す関数
 // 乱数シードを第2引数にとる
@@ -36,9 +35,9 @@ double get_itemweight(Item item);
 // Item の Valueを取得
 double get_itemvalue(Item item);
 
-//ファイルからItemsetを設定 [未実装, 課題1] 
+//ファイルからItemsetを設定 [未実装, 課題1]
 Itemset *load_itemset(char *filename);
 
-//Itemsetのパラメータを記録したバイナリファイルを出力する関数 [未実装, テスト用]
+// Itemsetのパラメータを記録したバイナリファイルを出力する関数 [未実装,
+// テスト用]
 void save_itemset(char *filename);
-
